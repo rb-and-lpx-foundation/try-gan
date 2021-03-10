38bb0553be6d03f905c7f7d1a5dd8323fccf5012
Reuben Brasher <rkbrasher@gmail.com>
2021-03-09 17:09:03 -0800

    Transform between one byte per channel and normalized float images

---
+++ try_gan/normalize.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def clip_floats(image, lower_bound=0.0, upper_bound=1.0):
+    image[image < lower_bound] = lower_bound
+    image[image > upper_bound] = upper_bound
+
+
+def normalize_to_floats(image):
+    return image.copy() / 255.0
+
+
+def normal_to_bytes(image):
+    return (image.copy() * 255).astype(np.uint8)
---
+++ try_gan/tests/test_normalize.py
@@ -0,0 +1,47 @@
+import unittest
+
+from numpy.linalg import norm
+import numpy as np
+
+from try_gan import normalize
+
+class TestNormalize(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_clip(self):
+        actual = np.array([[0, 1, 1], [-0.1, 0.5, 1.2]])
+        normalize.clip_floats(actual)
+        expected = np.array([[0.0, 1.0, 1.0], [0, 0.5, 1.0]])
+        self.assertAlmostEqual(0, norm(actual - expected))
+
+    def test_normalize_to_floats(self):
+        x = np.array([0, 128, 255])
+        y = normalize.normalize_to_floats(x)
+
+        # the original does not change
+        actual = x
+        expected = np.array([0, 128, 255])
+        self.assertAlmostEqual(0, norm(actual - expected))
+
+        # the new one is a normalized copy
+        actual = y
+        expected = np.array([0, 128.0 / 255.0, 1.0])
+        self.assertAlmostEqual(0, norm(actual - expected))
+
+    def test_normal_to_uint8(self):
+        x = np.array([0, 128.0 / 255.0, 1.0])
+        y = normalize.normal_to_bytes(x)
+
+        # the original does not change
+        actual = x
+        expected = np.array([0, 128.0 / 255.0, 1.0])
+        self.assertAlmostEqual(0, norm(actual - expected))
+
+        # the new one is a normalized copy
+        actual = y
+        expected = np.array([0, 128, 255])
+        self.assertAlmostEqual(0, norm(actual - expected))
