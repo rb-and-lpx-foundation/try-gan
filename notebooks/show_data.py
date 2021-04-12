3d1cd2d9276f717026f54a39f10c9ce92bb29584
Reuben Brasher <rkbrasher@gmail.com>
2021-04-12 10:16:28 -0700

    Show frames from a video

--- notebooks/show_data.py
+++ notebooks/show_data.py
@@ -1,21 +1,32 @@
 from matplotlib import pyplot as plt
 from IPython import display
+import numpy as np
+
+from try_gan import read_video
 
 
-def show_frames(video_frames, first_frame, n=10):
-    print(
-        "{} total frames at {} fps".format(video_frames.frame_count, video_frames.fps)
-    )
+def show_frames(frames: read_video.Frames, first_frame, n=10):
+    print("{} total frames at {} fps".format(frames.frame_count, frames.fps))
     frame = None
-    for i, frame in enumerate(video_frames.frames):
+    for i, frame in enumerate(frames.frames):
         if i >= first_frame:
             plt.figure()
             plt.imshow(frame)
         if i >= first_frame + n:
             break
-    video_frames.cleanup()
-    if video_frames.frame_count:
+    frames.cleanup()
+    if frames.frame_count:
         return frame
+
+
+def show_sampled_frames(frames: read_video.Frames, r: np.random.RandomState, n=10):
+    samples = read_video.sample_from_generator(frames, n, r)
+    frame = None
+    for frame in samples.frames:
+        plt.figure()
+        plt.imshow(frame)
+    frames.cleanup()
+    return frame
 
 
 def generate_images(model, test_input, tar):
@@ -34,9 +45,9 @@ def generate_images(model, test_input, tar):
     plt.show()
 
 
-def show_images(test_ds, epoch):
+def show_cdgan_images(model, test_ds, epoch):
     display.clear_output(wait=True)
 
     for example_input, example_target in test_ds.take(2):
-        generate_images(pix2pix.generator, example_input, example_target)
+        generate_images(model.generator, example_input, example_target)
     print("Epoch: ", epoch)
